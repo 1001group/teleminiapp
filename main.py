c6c5f3cdd543b1e130491ba496e5458dcd16768e
lauren melua <165530663+1001group@users.noreply.github.com>
2024-12-13 11:22:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ TOKEN = '7590187559:AAG3LzMwLj-koPQ_t6Z3IhxpgT0ud0bRMwM'
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     keyboard = [
-        [InlineKeyboardButton("Visit Our Mini App", url="https://github.com/1001group/teleminiapp.git")]
+        [InlineKeyboardButton("Visit Our Mini App", url="https://github.com/1001group/teleminiapp/index.html")]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(f"Hello {user.first_name}, welcome to our Telegram bot!\n"
